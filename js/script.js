abe8c088884baf9f09c0e11445cea17010006cb1
Nils Petter Ellingsen <nils@deviant.no>
2012-02-07 10:25:35 +0100

    merging upstream changes from Roots theme (cross fingers)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 /* Author:
-q2i@storebrand.no  ( https://github.com/nilsel/stb-roots/ )
-// STBMOD
+
 */
 
 
-// popover (big tooltips)
-$("a[rel=popover]").popover().click(function(e) {
-	e.preventDefault()
-});
 
-// tooltip demo
-/////$('a[rel=tooltip]').tooltip();
+
--- loop-page.php
+++ loop-page.php
@@ -2,11 +2,11 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php roots_post_before(); ?>
     <?php roots_post_inside_before(); ?>
-    	<div class="well">
-      <h1><?php the_title(); ?></h1>
-      	<?php the_content(__("<br/>Les mer...", "roots")); ?>
-      	<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-			</div>
+      <div class="page-header">
+      	<h1><?php the_title(); ?></h1>
+      </div>
+      <?php the_content(); ?>
+      <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
   <?php roots_post_after(); ?>
 <?php endwhile; /* End loop */ ?>
