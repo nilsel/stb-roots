0b5f33c619a5df8e6238efc1c57ac401f5000653
Nils Petter Ellingsen <nils@deviant.no>
2012-02-07 16:21:00 +0100

    Adjusted frontpage-loop (css)

--- loop-page.php
+++ loop-page.php
@@ -5,7 +5,7 @@
       <div class="page-header">
       	<h1><?php the_title(); ?></h1>
       </div>
-      <?php the_content(); ?>
+      <?php the_content('<br/>Les mer...'); ?>
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
   <?php roots_post_after(); ?>
