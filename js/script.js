022a48d681634b9936035cc7d36fa2eb924a1244
Nils Petter Ellingsen <nils@deviant.no>
2012-02-08 13:46:20 +0100

    Added js-code to init tipsy popovers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 /* Author:
-
+	q2i@storebrand.no  ( https://github.com/nilsel/stb-roots/ )
+// STBMOD
 */
 
+$(function(){
+// popover (big tooltips)
+$("a[rel=popover]").popover().click(function(e) {
+	e.preventDefault()
+});
+
+// tooltip demo
+/////$('a[rel=tooltip]').tooltip();
 
 
-
+});
