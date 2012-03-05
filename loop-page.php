88b9700b6ab475a0879941e529b752a2d1628579
Nils Petter Ellingsen <nils@deviant.no>
2012-03-05 16:53:21 +0100

    wrapped post title in link to post

--- loop-page.php
+++ loop-page.php
@@ -1,12 +1,18 @@
+        <div class="well">
+
 <?php /* Start loop */ ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php roots_post_before(); ?>
+  <div class="welL">
     <?php roots_post_inside_before(); ?>
       <div class="page-header">
-      	<h1><?php the_title(); ?></h1>
+      	<a href="<?php the_permalink();?>"><h1><?php the_title(); ?></h1></a>
       </div>
       <?php the_content('<br/>Les mer...'); ?>
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
+    </div>
   <?php roots_post_after(); ?>
 <?php endwhile; /* End loop */ ?>
+
+</div>
