61a46657ff20de3e18bfb83ee6f5ca161b4b7b5c
Nils Petter Ellingsen <nils@deviant.no>
2012-03-06 10:33:23 +0100

    no need for well-div here + added post date in <h6>

--- loop-page.php
+++ loop-page.php
@@ -3,15 +3,14 @@
 <?php /* Start loop */ ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php roots_post_before(); ?>
-  <div class="welL">
     <?php roots_post_inside_before(); ?>
       <div class="page-header">
       	<a href="<?php the_permalink();?>"><h1><?php the_title(); ?></h1></a>
+      	<h6 class="pull-right"><?php the_date();?> <?php the_time(); ?></h6>
       </div>
       <?php the_content('<br/>Les mer...'); ?>
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
-    </div>
   <?php roots_post_after(); ?>
 <?php endwhile; /* End loop */ ?>
 
