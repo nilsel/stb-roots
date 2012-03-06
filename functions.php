e180f20fb054f5e02fa0c32299fe5c24c941ed2c
Nils Petter Ellingsen <nils@deviant.no>
2012-03-06 10:35:45 +0100

    edited the roots_entry_meta to output post date&time

--- functions.php
+++ functions.php
@@ -71,6 +71,6 @@ add_action('widgets_init', 'roots_register_sidebars');
 
 // return post entry meta information
 function roots_entry_meta() {
-  echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()) .'</time>';
+  echo '<h6 class="pull-right"><time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()) .'</time></h6>';
   echo '<p class="byline author vcard">'. __('Written by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('id')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
 }
